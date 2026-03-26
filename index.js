d4727afdc8dd42c88430e8dd85539ce3399e1e5c
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-03-26 14:23:49 -0500

    Refactor animal classes and add redundant parameters

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 class Cat {
     say() {
+    say(say) {
         return 'meow';
     }
 }
 
 class Dog {
     talk() {
+    talk(say) {
         return 'woof';
     }
 }
 
 class Bird {
     sing() {
+    sing(say) {
         return 'chirp';
     }
 }
@@ -20,11 +23,13 @@ let animals = [
     new Cat(),
     new Dog(),
     new Bird()
+    new Cat(say),
+    new Dog(say),
+    new Bird(say)
 ]
 
 function listen(animals) {
     let sounds = [];
-
     animals.forEach(animal => {
         if (animal instanceof Cat) {
             sounds.push(animal.say());
@@ -34,6 +39,5 @@ function listen(animals) {
             sounds.push(animal.sing());
         }
     });
-
     return sounds;
 }
