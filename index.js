05910da69090e3dbe52a51ee92ea453ced1caa94
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-03-27 14:45:01 -0500

    Refactor animal classes and fix syntax errors

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 class Cat {
     say() {
-    say(say) {
         return 'meow';
     }
 }
 
 class Dog {
     talk() {
-    talk(say) {
         return 'woof';
     }
 }
 
 class Bird {
     sing() {
-    sing(say) {
         return 'chirp';
     }
 }
@@ -23,13 +20,11 @@ let animals = [
     new Cat(),
     new Dog(),
     new Bird()
-    new Cat(say),
-    new Dog(say),
-    new Bird(say)
 ]
 
 function listen(animals) {
     let sounds = [];
+    
     animals.forEach(animal => {
         if (animal instanceof Cat) {
             sounds.push(animal.say());
